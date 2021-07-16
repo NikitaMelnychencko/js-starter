18c728befc7f847edfe789123ab3bd62237d93ab
NikitaMelnychencko <hunter6059@gmail.com>
2021-07-16 21:36:44 +0300

    Primitive values to String

--- src/js/primitive_ values_ to_string.js
+++ src/js/primitive_ values_ to_string.js
@@ -8,4 +8,113 @@ let value = 0;
 
 // 1-th lap
 let toString = String(value);
-console.log(`${ value }, through the constructor String):` , toString);
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+// 2-th lap
+toString = value + "";
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+
+ value = 1;
+
+// 1-th lap
+toString = String(value);
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+// 2-th lap
+toString = value + "";
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+
+//Infinity
+
+ value = Infinity;
+
+// 1-th lap
+toString = String(value);
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+// 2-th lap
+toString = value + "";
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+//Nan
+
+ value = NaN;
+
+// 1-th lap
+toString = String(value);
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+// 2-th lap
+toString = value + "";
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+//Boolean(logic) true to string
+
+ value = true;
+
+// 1-th lap
+toString = String(value);
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+// 2-th lap
+toString = value + "";
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+
+//false to string
+
+ value = false;
+
+// 1-th lap
+toString = String(value);
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+// 2-th lap
+toString = value + "";
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+
+
+//undefined to string
+
+ value = undefined;
+
+// 1-th lap
+toString = String(value);
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+// 2-th lap
+toString = value + "";
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+
+
+//null to string
+
+ value = null;
+
+// 1-th lap
+toString = String(value);
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
+
+// 2-th lap
+toString = value + "";
+console.log(`${value}, through the constructor String():`, toString);
+console.log(`type data ${value}, through the constructor String():`, typeof toString);
